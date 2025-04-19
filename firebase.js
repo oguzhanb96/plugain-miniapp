b70d626ba10e5b3aabd7a8a5761771297bb053d2
oguzhanb96 <bacakoguzhan@gmail.com>
2025-04-19 20:12:23 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,30 @@
+// Firebase SDK'yı import et
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
+
+// Firebase konfigürasyonu
+const firebaseConfig = {
+  apiKey: "AIzaSyBwwATHKjJbwUQT7sNEHl-fVJUAN0mNqpk",
+  authDomain: "plugain.firebaseapp.com",
+  databaseURL: "https://plugain-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "plugain",
+  storageBucket: "plugain.firebasestorage.app",
+  messagingSenderId: "693636169295",
+  appId: "1:693636169295:web:fc965cea5bfe7b88bcb43a",
+  measurementId: "G-ENH4WNHVE4"
+};
+
+// Firebase'i başlat
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+// Kullanıcı verisi kaydetme fonksiyonu
+export function saveUserData(userId, username, chatId) {
+  return set(ref(database, 'users/' + userId), {
+    username: username,
+    chatId: chatId,
+    createdAt: new Date().toISOString()
+  });
+}
